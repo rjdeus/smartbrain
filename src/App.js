44e01f3f3f1700fcb07c41510709ee5f7f261b31
rjdeus <41559152+rjdeus@users.noreply.github.com>
2018-12-31 23:42:28 -0500

    implementing facial recognition api

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Clarifai from 'clarifai';
 import './App.css';
+
+const app = new Clarifai.App({
+  //add api key apiKey: 
+ });
 
 const particlesOptions ={
   particles: {
@@ -17,8 +23,36 @@ const particlesOptions ={
     }
   }
 }
-
+ 
 class App extends Component {
+  constructor() {
+    super();
+    this.state={
+      input:'',
+      imageUrl:''
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value})
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    app.models
+    .predict(
+      Clarifai.FACE_DETECT_MODEL, 
+      this.state.input)
+    .then(function(response) {
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+        // do something with response
+      },
+      function(err) {
+        // there was an error
+      }
+    );
+  }
+
   render() {
     return (
       <div className="App">
@@ -28,9 +62,10 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/* 
-        <FaceRecognition /> */}
+        <ImageLinkForm 
+        onInputChange={this.onInputChange} 
+        onButtonSubmit={this.onButtonSubmit}/>
+        <FaceRecognition imageUrl={this.state.imageUrl} />
       </div>
     );
   }
